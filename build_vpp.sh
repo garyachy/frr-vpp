562675c3c00b43107f5fd3616c2d0eaa3c953c3d
Denys Haryachyy <garyachy@gmail.com>
2020-04-03 11:00:42 +0000

    Clone.

--- build_vpp.sh
+++ build_vpp.sh
@@ -5,20 +5,22 @@ set -e  # exit on the first command failure
 apt-get install -y python-cffi python-pycparser
 
 git clone https://github.com/garyachy/vppsb.git
-git clone https://github.com/FDio/vpp
+cd vppsb
+git checkout compile_problems
+cd -
 
+git clone https://github.com/FDio/vpp
 cd vpp
 git checkout stable/2001
 cd -
-
-rm vpp/src/plugins/netlink
-rm vpp/src/plugins/router
 
 ln -sf ../../../vppsb/netlink vpp/src/plugins/netlink
 ln -sf ../../../vppsb/router vpp/src/plugins/router
 
 cp startup.conf vpp/src/vpp/conf/
 cp init.txt vpp/src/vpp/conf/
+
+sed -i '$ a install(FILES conf/init.txt DESTINATION etc/vpp COMPONENT vpp)' vpp/src/vpp/CMakeLists.txt
 
 cd vpp
 
